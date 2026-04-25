b5137d70f93b1db6ff3e383c8ea62a6a52d54648
Kvsna3545 <kvsna3545@gmail.com>
2026-04-25 09:45:23 +0700

    Create Bwiw.lua

---
+++ Bwiw.lua
@@ -0,0 +1,333 @@
+--[=[
+ d888b  db    db d888888b      .d888b.      db      db    db  .d8b.  
+88' Y8b 88    88   `88'        VP  `8D      88      88    88 d8' `8b 
+88      88    88    88            odD'      88      88    88 88ooo88 
+88  ooo 88    88    88          .88'        88      88    88 88~~~88 
+88. ~8~ 88b  d88   .88.        j88.         88booo. 88b  d88 88   88    @uniquadev
+ Y888P  ~Y8888P' Y888888P      888888D      Y88888P ~Y8888P' YP   YP  CONVERTER 
+
+designed using localmaze gui creator
+]=]
+
+-- Instances: 16 | Scripts: 0 | Modules: 0 | Tags: 0
+local LMG2L = {};
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui
+LMG2L["ScreenGui_1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+LMG2L["ScreenGui_1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame
+LMG2L["Frame_2"] = Instance.new("Frame", LMG2L["ScreenGui_1"]);
+LMG2L["Frame_2"]["BorderSizePixel"] = 0;
+LMG2L["Frame_2"]["BackgroundColor3"] = Color3.fromRGB(0, 0, 0);
+LMG2L["Frame_2"]["Size"] = UDim2.new(0, 580, 0, 312);
+LMG2L["Frame_2"]["Position"] = UDim2.new(0, 162, 0, 2);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextButton9
+LMG2L["TextButton9_3"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["TextButton9_3"]["TextWrapped"] = true;
+LMG2L["TextButton9_3"]["BorderSizePixel"] = 0;
+LMG2L["TextButton9_3"]["TextSize"] = 13;
+LMG2L["TextButton9_3"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton9_3"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton9_3"]["FontFace"] = Font.new([[rbxasset://fonts/families/PressStart2P.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+LMG2L["TextButton9_3"]["Size"] = UDim2.new(0, 140, 0, 40);
+LMG2L["TextButton9_3"]["Text"] = [[project baller]];
+LMG2L["TextButton9_3"]["Name"] = [[TextButton9]];
+LMG2L["TextButton9_3"]["Style"] = Enum.ButtonStyle.RobloxButtonDefault;
+LMG2L["TextButton9_3"]["Position"] = UDim2.new(0, 436, 0, 192);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextButton7
+LMG2L["TextButton7_4"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["TextButton7_4"]["TextWrapped"] = true;
+LMG2L["TextButton7_4"]["BorderSizePixel"] = 0;
+LMG2L["TextButton7_4"]["TextSize"] = 11;
+LMG2L["TextButton7_4"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton7_4"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton7_4"]["FontFace"] = Font.new([[rbxasset://fonts/families/PressStart2P.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+LMG2L["TextButton7_4"]["Size"] = UDim2.new(0, 140, 0, 40);
+LMG2L["TextButton7_4"]["Text"] = [[Thank you Project]];
+LMG2L["TextButton7_4"]["Name"] = [[TextButton7]];
+LMG2L["TextButton7_4"]["Style"] = Enum.ButtonStyle.RobloxButtonDefault;
+LMG2L["TextButton7_4"]["Position"] = UDim2.new(0, 436, 0, 120);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextButton2
+LMG2L["TextButton2_5"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["TextButton2_5"]["TextWrapped"] = true;
+LMG2L["TextButton2_5"]["BorderSizePixel"] = 0;
+LMG2L["TextButton2_5"]["TextSize"] = 22;
+LMG2L["TextButton2_5"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton2_5"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton2_5"]["FontFace"] = Font.new([[rbxasset://fonts/families/PressStart2P.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+LMG2L["TextButton2_5"]["Size"] = UDim2.new(0, 116, 0, 40);
+LMG2L["TextButton2_5"]["Text"] = [[Clear]];
+LMG2L["TextButton2_5"]["Name"] = [[TextButton2]];
+LMG2L["TextButton2_5"]["Style"] = Enum.ButtonStyle.RobloxButtonDefault;
+LMG2L["TextButton2_5"]["Position"] = UDim2.new(0, 126, 0, 268);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextButton3
+LMG2L["TextButton3_6"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["TextButton3_6"]["TextWrapped"] = true;
+LMG2L["TextButton3_6"]["BorderSizePixel"] = 0;
+LMG2L["TextButton3_6"]["TextSize"] = 22;
+LMG2L["TextButton3_6"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton3_6"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton3_6"]["FontFace"] = Font.new([[rbxasset://fonts/families/PressStart2P.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+LMG2L["TextButton3_6"]["Size"] = UDim2.new(0, 116, 0, 40);
+LMG2L["TextButton3_6"]["Text"] = [[Load]];
+LMG2L["TextButton3_6"]["Name"] = [[TextButton3]];
+LMG2L["TextButton3_6"]["Style"] = Enum.ButtonStyle.RobloxButtonDefault;
+LMG2L["TextButton3_6"]["Position"] = UDim2.new(0, 242, 0, 268);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextButton
+LMG2L["TextButton_7"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["TextButton_7"]["TextWrapped"] = true;
+LMG2L["TextButton_7"]["BorderSizePixel"] = 0;
+LMG2L["TextButton_7"]["TextSize"] = 22;
+LMG2L["TextButton_7"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton_7"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton_7"]["FontFace"] = Font.new([[rbxasset://fonts/families/PressStart2P.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+LMG2L["TextButton_7"]["Size"] = UDim2.new(0, 116, 0, 40);
+LMG2L["TextButton_7"]["Text"] = [[execute]];
+LMG2L["TextButton_7"]["Style"] = Enum.ButtonStyle.RobloxButtonDefault;
+LMG2L["TextButton_7"]["Position"] = UDim2.new(0, 8, 0, 268);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextButton4
+LMG2L["TextButton4_8"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["TextButton4_8"]["TextWrapped"] = true;
+LMG2L["TextButton4_8"]["BorderSizePixel"] = 0;
+LMG2L["TextButton4_8"]["TextSize"] = 22;
+LMG2L["TextButton4_8"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton4_8"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton4_8"]["FontFace"] = Font.new([[rbxasset://fonts/families/PressStart2P.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+LMG2L["TextButton4_8"]["Size"] = UDim2.new(0, 76, 0, 40);
+LMG2L["TextButton4_8"]["Text"] = [[R6]];
+LMG2L["TextButton4_8"]["Name"] = [[TextButton4]];
+LMG2L["TextButton4_8"]["Style"] = Enum.ButtonStyle.RobloxButtonDefault;
+LMG2L["TextButton4_8"]["Position"] = UDim2.new(0, 356, 0, 268);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextButton11
+LMG2L["TextButton11_9"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["TextButton11_9"]["TextWrapped"] = true;
+LMG2L["TextButton11_9"]["BorderSizePixel"] = 0;
+LMG2L["TextButton11_9"]["TextSize"] = 22;
+LMG2L["TextButton11_9"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton11_9"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton11_9"]["FontFace"] = Font.new([[rbxasset://fonts/families/PressStart2P.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+LMG2L["TextButton11_9"]["Size"] = UDim2.new(0, 140, 0, 40);
+LMG2L["TextButton11_9"]["Text"] = [[coolgui]];
+LMG2L["TextButton11_9"]["Name"] = [[TextButton11]];
+LMG2L["TextButton11_9"]["Style"] = Enum.ButtonStyle.RobloxButtonDefault;
+LMG2L["TextButton11_9"]["Position"] = UDim2.new(0, 438, 0, 268);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextButton10
+LMG2L["TextButton10_a"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["TextButton10_a"]["TextWrapped"] = true;
+LMG2L["TextButton10_a"]["BorderSizePixel"] = 0;
+LMG2L["TextButton10_a"]["TextSize"] = 22;
+LMG2L["TextButton10_a"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton10_a"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton10_a"]["FontFace"] = Font.new([[rbxasset://fonts/families/PressStart2P.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+LMG2L["TextButton10_a"]["Size"] = UDim2.new(0, 140, 0, 40);
+LMG2L["TextButton10_a"]["Text"] = [[koopgui]];
+LMG2L["TextButton10_a"]["Name"] = [[TextButton10]];
+LMG2L["TextButton10_a"]["Style"] = Enum.ButtonStyle.RobloxButtonDefault;
+LMG2L["TextButton10_a"]["Position"] = UDim2.new(0, 436, 0, 228);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextButton6
+LMG2L["TextButton6_b"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["TextButton6_b"]["TextWrapped"] = true;
+LMG2L["TextButton6_b"]["BorderSizePixel"] = 0;
+LMG2L["TextButton6_b"]["TextSize"] = 22;
+LMG2L["TextButton6_b"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton6_b"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton6_b"]["FontFace"] = Font.new([[rbxasset://fonts/families/PressStart2P.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+LMG2L["TextButton6_b"]["Size"] = UDim2.new(0, 140, 0, 40);
+LMG2L["TextButton6_b"]["Text"] = [[dex]];
+LMG2L["TextButton6_b"]["Name"] = [[TextButton6]];
+LMG2L["TextButton6_b"]["Style"] = Enum.ButtonStyle.RobloxButtonDefault;
+LMG2L["TextButton6_b"]["Position"] = UDim2.new(0, 436, 0, 84);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextButton5
+LMG2L["TextButton5_c"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["TextButton5_c"]["TextWrapped"] = true;
+LMG2L["TextButton5_c"]["BorderSizePixel"] = 0;
+LMG2L["TextButton5_c"]["TextSize"] = 22;
+LMG2L["TextButton5_c"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton5_c"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton5_c"]["FontFace"] = Font.new([[rbxasset://fonts/families/PressStart2P.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+LMG2L["TextButton5_c"]["Size"] = UDim2.new(0, 140, 0, 40);
+LMG2L["TextButton5_c"]["Text"] = [[re]];
+LMG2L["TextButton5_c"]["Name"] = [[TextButton5]];
+LMG2L["TextButton5_c"]["Style"] = Enum.ButtonStyle.RobloxButtonDefault;
+LMG2L["TextButton5_c"]["Position"] = UDim2.new(0, 436, 0, 48);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextButton8
+LMG2L["TextButton8_d"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["TextButton8_d"]["TextWrapped"] = true;
+LMG2L["TextButton8_d"]["BorderSizePixel"] = 0;
+LMG2L["TextButton8_d"]["TextSize"] = 22;
+LMG2L["TextButton8_d"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton8_d"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextButton8_d"]["FontFace"] = Font.new([[rbxasset://fonts/families/PressStart2P.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+LMG2L["TextButton8_d"]["Size"] = UDim2.new(0, 140, 0, 40);
+LMG2L["TextButton8_d"]["Text"] = [[rc7]];
+LMG2L["TextButton8_d"]["Name"] = [[TextButton8]];
+LMG2L["TextButton8_d"]["Style"] = Enum.ButtonStyle.RobloxButtonDefault;
+LMG2L["TextButton8_d"]["Position"] = UDim2.new(0, 436, 0, 156);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextLabel
+LMG2L["TextLabel_e"] = Instance.new("TextLabel", LMG2L["Frame_2"]);
+LMG2L["TextLabel_e"]["TextWrapped"] = true;
+LMG2L["TextLabel_e"]["BorderSizePixel"] = 0;
+LMG2L["TextLabel_e"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+LMG2L["TextLabel_e"]["TextYAlignment"] = Enum.TextYAlignment.Top;
+LMG2L["TextLabel_e"]["TextScaled"] = true;
+LMG2L["TextLabel_e"]["BackgroundColor3"] = Color3.fromRGB(33, 33, 33);
+LMG2L["TextLabel_e"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextLabel_e"]["Size"] = UDim2.new(0, 520, 0, 36);
+LMG2L["TextLabel_e"]["Text"] = [[victim by kvsna3545 ]];
+LMG2L["TextLabel_e"]["Position"] = UDim2.new(0, 54, 0, 6);
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.TextBox
+LMG2L["TextBox_f"] = Instance.new("TextBox", LMG2L["Frame_2"]);
+LMG2L["TextBox_f"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+LMG2L["TextBox_f"]["PlaceholderColor3"] = Color3.fromRGB(228, 228, 228);
+LMG2L["TextBox_f"]["BorderSizePixel"] = 2;
+LMG2L["TextBox_f"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextBox_f"]["TextYAlignment"] = Enum.TextYAlignment.Top;
+LMG2L["TextBox_f"]["BackgroundColor3"] = Color3.fromRGB(0, 0, 0);
+LMG2L["TextBox_f"]["Selectable"] = false;
+LMG2L["TextBox_f"]["ClearTextOnFocus"] = false;
+LMG2L["TextBox_f"]["PlaceholderText"] = [[script by kvsna3545
+
+
+:/ I am not ok]];
+LMG2L["TextBox_f"]["Size"] = UDim2.new(0, 422, 0, 212);
+LMG2L["TextBox_f"]["Position"] = UDim2.new(0, 8, 0, 50);
+LMG2L["TextBox_f"]["BorderColor3"] = Color3.fromRGB(255, 0, 0);
+LMG2L["TextBox_f"]["Text"] = [[]];
+
+
+-- Players.giruoh_jgry.PlayerGui.ScreenGui.Frame.ImageLabel
+LMG2L["ImageLabel_10"] = Instance.new("ImageLabel", LMG2L["Frame_2"]);
+LMG2L["ImageLabel_10"]["BorderSizePixel"] = 0;
+LMG2L["ImageLabel_10"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+-- LMG2L["ImageLabel_10"]["ImageContent"] = ;
+LMG2L["ImageLabel_10"]["Image"] = [[rbxassetid://73185495890754]];
+LMG2L["ImageLabel_10"]["Size"] = UDim2.new(0, 56, 0, 48);
+LMG2L["ImageLabel_10"]["BackgroundTransparency"] = 1;
+
+local UIS = game:GetService("UserInputService")
+
+local textbox = LMG2L["TextBox_f"]
+local executeBtn = LMG2L["TextButton_7"]
+local clearBtn = LMG2L["TextButton2_5"]
+local loadBtn = LMG2L["TextButton3_6"]
+local frame = LMG2L["Frame_2"]
+
+-- =======================
+-- 🖱️ DRAG GUI
+-- =======================
+local dragging = false
+local dragInput
+local dragStart
+local startPos
+
+local function update(input)
+    local delta = input.Position - dragStart
+    frame.Position = UDim2.new(
+        startPos.X.Scale,
+        startPos.X.Offset + delta.X,
+        startPos.Y.Scale,
+        startPos.Y.Offset + delta.Y
+    )
+end
+
+frame.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 then
+        dragging = true
+        dragStart = input.Position
+        startPos = frame.Position
+
+        input.Changed:Connect(function()
+            if input.UserInputState == Enum.UserInputState.End then
+                dragging = false
+            end
+        end)
+    end
+end)
+
+frame.InputChanged:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseMovement then
+        dragInput = input
+    end
+end)
+
+UIS.InputChanged:Connect(function(input)
+    if input == dragInput and dragging then
+        update(input)
+    end
+end)
+
+-- =======================
+-- ⚡ EXECUTE
+-- =======================
+executeBtn.MouseButton1Click:Connect(function()
+    local code = textbox.Text
+    
+    local func, err = loadstring(code)
+    if func then
+        local success, result = pcall(func)
+        if not success then
+            warn("Runtime error: "..result)
+        end
+    else
+        warn("Compile error: "..err)
+    end
+end)
+
+-- =======================
+-- 🧹 CLEAR
+-- =======================
+clearBtn.MouseButton1Click:Connect(function()
+    textbox.Text = ""
+end)
+
+-- =======================
+-- 📥 LOAD (SAFE)
+-- =======================
+loadBtn.MouseButton1Click:Connect(function()
+    local success, result = pcall(function()
+        return game:HttpGet("https://pastebin.com/raw/Kz8q3GyA")
+    end)
+
+    if success then
+        local func, err = loadstring(result)
+        if func then
+            pcall(func)
+        else
+            warn("Loadstring error: "..err)
+        end
+    else
+        warn("HttpGet failed")
+    end
+end)
+
+-- เซฟไว้ก่อน555
+
+return LMG2L["ScreenGui_1"], require;
